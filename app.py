f39cecf103ab4c10b097f13fb40c1f1be83ea2d2
unknown <krauskas@gmail.com>
2021-03-04 17:02:37 +0200

    change app to send msg to hagai

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-# import pywhatkit
+import pywhatkit
 
 
-# def send_greet(contact_num: str):
+def send_greet(contact_num: str):
 
-#     msg = 'כבר אמרו לך שאתה אהוב וטוב מספיק היום? ;)'
-#     hours = 13
-#     minutes = 15
-#     pywhatkit.sendwhatmsg(contact_num, msg, hours, minutes)
-#     print("hello world")
-
-
-# hagai = '+972546995552'
-# barak = '+972504081472'
-# send_greet(hagai)
-
-def test():
-    for i in range(10):
-        print(i)
+    msg = 'כבר אמרו לך שאתה אהוב וטוב מספיק היום? ;)'
+    hours = 13
+    minutes = 15
+    pywhatkit.sendwhatmsg(contact_num, msg, hours, minutes)
+    print("hello world")
 
 
-test()
+hagai = '+972546995552'
+barak = '+972504081472'
+send_greet(hagai)
+
+# def test():
+#     for i in range(10):
+#         print(i)
+
+
+# test()
